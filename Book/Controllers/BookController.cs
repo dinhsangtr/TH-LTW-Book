c1899a3caaaa88de669364eb733d9a7eadc6c811
D.S <sangtruong2811@gmail.com>
2021-06-29 10:33:43 +0700

    Add project files.

---
+++ Book/Controllers/BookController.cs
@@ -0,0 +1,146 @@
+﻿using Book.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Book.Controllers
+{
+    public class BookController : Controller
+    {
+        BookManagerContext context = new BookManagerContext();
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: ListBook
+        public ActionResult ListBook()
+        {
+            var listBook = context.Books.ToList();
+            return View(listBook);
+        }
+
+        // BUY
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            Models.Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // CREATE
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID, Title, Description, Author, Images, Price")] Models.Book book)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    context.Books.AddOrUpdate(book);
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Error Save Data");
+            }
+            context.SaveChanges();
+            var books = context.Books.ToList();
+            return View("ListBook", books);
+        }
+
+
+
+        // EDIT: Book
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            Models.Book book = context.Books.FirstOrDefault(x => x.ID == id);
+            if (book == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(book);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Models.Book book)
+        {
+            Models.Book book1 = context.Books.SingleOrDefault(x => x.ID == id);
+            book1.Title = book.Title;
+            book1.Description = book.Description;
+            book1.Author = book.Author;
+            book1.Images = book.Images;
+            book1.Price = book.Price;
+            context.Books.AddOrUpdate(book1);
+            context.SaveChanges();
+            var books = context.Books.ToList();
+            return View("ListBook", books);
+        }
+
+        [Authorize]
+        // DELETE: Book
+        public ActionResult Delete(int id)
+        {
+            Models.Book book = context.Books.FirstOrDefault(x => x.ID == id);
+            ViewBag.IDBook = book.ID;
+            if (book == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteBook(int id)
+        {
+            Models.Book book = context.Books.FirstOrDefault(x => x.ID == id);
+            ViewBag.IDBook = book.ID;
+            if (book == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            context.Books.Remove(book);
+            context.SaveChanges();
+            return RedirectToAction("ListBook");
+        }
+
+
+
+
+        public ActionResult Details(int id)
+        {
+            Models.Book book = context.Books.FirstOrDefault(x => x.ID == id);
+            if (book == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(book);
+        }
+    }
+}
---
+++ Book/Models/Book.cs
@@ -0,0 +1,48 @@
+﻿namespace Book.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Mã sách")]
+        [Required(ErrorMessage = "ID không được để trống")]
+        public int ID { get; set; }
+
+
+        [Display(Name = "Tên sách")]
+        [Required(ErrorMessage = "Tên sách không được để trống")]
+        [StringLength(100, ErrorMessage = "Tên sách không được quá 100 ký tự.")]
+        public string Title { get; set; }
+
+
+        [Display(Name = "Mô tả")]
+        [Required(ErrorMessage = "Mô tả không được để trống")]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+
+        [Display(Name = "Tác giả")]
+        [Required(ErrorMessage = "Tên tác giả không được để trống")]
+        [StringLength(30, ErrorMessage = "Tên sách không được quá 30 ký tự.")]
+        public string Author { get; set; }
+
+
+        [Display(Name = "Hình ảnh")]
+        [Required(ErrorMessage = "Hình ảnh không được để trống")]
+        [StringLength(255)]
+        public string Images { get; set; }
+
+
+        [Display(Name = "Giá")]
+        [Required(ErrorMessage = "Giá không được để trống")]
+        [Range(1000, 1000000, ErrorMessage = "Giá sách nằm trong khoảng từ 1.000 - 1.000.000")]
+        public int Price { get; set; }
+    }
+}
